982193fdc31b3644e02e471d0f57302663c43b7d
dan <you@example.com>
2022-10-16 23:57:26 -0500

    primera version

--- Demo_Django/settings.py
+++ Demo_Django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d3t4a0*-5+l@z#@9zttnp8f8hrk(uyy19s-zo!++(2s2g9eocf'
+#SECRET_KEY = 'django-insecure-d3t4a0*-5+l@z#@9zttnp8f8hrk(uyy19s-zo!++(2s2g9eocf'
+KEY_DIR = os.environ['SECRET_KEY']
+with open(KEY_DIR,'rb') as f:
+    SECRET_KEY = f.read().strip().decode()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'Demo_Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'app/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'app/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
